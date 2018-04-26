776be1a706a3b84294c3978d947944135ffe9912
Ossi Rönnberg <ossi.ronnberg@gmail.com>
2018-04-26 14:39:50 +0300

    Removed: debugger.

--- chartjs-plugin-annotation.js
+++ chartjs-plugin-annotation.js
@@ -678,7 +678,6 @@ module.exports = function(Chart) {
       model.ranges = {};
     },
     configure: function() {
-      debugger;
       var model = this._model;
       var options = this.options;
       var chartInstance = this.chartInstance;
